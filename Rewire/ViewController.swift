619916858b523dcf7e8abf86d314708d8b0cb7a1
zzzel <marko@zzzel.xyz>
2016-03-23 00:19:51 +0100

    Presenting alert if no habit is being tracked.

--- Rewire/ViewController.swift
+++ Rewire/ViewController.swift
@@ -13,13 +13,36 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		NSUserDefaults.standardUserDefaults().setValue(NSDate(), forKey: "startDate")
-		print(NSUserDefaults.standardUserDefaults().valueForKey("startDate"))
+		NSUserDefaults.standardUserDefaults().removeObjectForKey("startDate")
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		if let startDate = NSUserDefaults.standardUserDefaults().valueForKey("startDate") {
+			print(startDate)
+		} else {
+			let alert = UIAlertController(title: "✋", message: "No habit progress is being tracked,\nadd a new habit to start.", preferredStyle: .Alert)
+			
+			var habitTextField: UITextField!
+			alert.addTextFieldWithConfigurationHandler({ (textField) in
+				textField.placeholder = "enter habit name"
+				habitTextField = textField
+			})
+			
+			alert.addAction(UIAlertAction(title: "Done", style: .Default, handler: { (action) in
+				NSUserDefaults.standardUserDefaults().setValue(NSDate(), forKey: "startDate")
+				if habitTextField.text!.isEmpty {
+					self.title = "Habit Name"
+				} else {
+					self.title = habitTextField.text!
+				}
+			}))
+			
+			alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+			
+			presentViewController(alert, animated: true, completion: nil)
+		}
 	}
 
 
