21eb9601a440cb84820483f1476017260a0307af
zzzel <marko@zzzel.xyz>
2016-06-23 00:00:08 +0200

    dispatched HUD actions on main queue

--- Rewire/Presentable.swift
+++ Rewire/Presentable.swift
@@ -16,11 +16,17 @@ extension Presentable {
 	func show(type: HUDType) {
 		switch type {
 		case .success(let text):
-			HUD.flash(.LabeledSuccess(title: text, subtitle: nil))
+			dispatch_async(dispatch_get_main_queue()) {
+				HUD.flash(.LabeledSuccess(title: text, subtitle: nil), delay: 1.0)
+			}
 		case .error(let errorMessage):
-			HUD.flash(.LabeledError(title: errorMessage, subtitle: nil))
+			dispatch_async(dispatch_get_main_queue()) {
+				HUD.flash(.LabeledError(title: errorMessage, subtitle: nil), delay: 1.0)
+			}
 		case .progress(let text):
-			HUD.show(.LabeledProgress(title: text, subtitle: nil))
+			dispatch_async(dispatch_get_main_queue()) {
+				HUD.show(.LabeledProgress(title: text, subtitle: nil))
+			}
 		}
 	}
 }
---
+++ RewireTests/TwitterAccountRequestableTests.swift
@@ -0,0 +1,23 @@
+//
+//  TwitterAccountRequestableTests.swift
+//  Rewire
+//
+//  Created by Zel Marko on 6/22/16.
+//  Copyright © 2016 Zel Marko. All rights reserved.
+//
+
+import XCTest
+@testable import Rewire
+
+class TwitterAccountRequestableTests: XCTestCase {
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+	
+	
+}
